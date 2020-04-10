f81c29142714bd52a8875e2296e6e47604f57ebe
hamzarajaa <rh408905@gmail.com>
2020-04-11 01:50:58 +0500

    second commit

---
+++ src/db/DbConnection.java
@@ -0,0 +1,93 @@
+package db;
+
+import java.sql.*;
+
+/**
+ * Created by hamza raja on 11/4/2020.
+ */
+public class DbConnection {
+    private String dbURL = "jdbc:mysql://localhost:3306/users";
+    private String username = "root";
+    private String password = "";
+    private Connection connection;
+    public DbConnection(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(dbURL,username,password);
+            if(connection!=null){
+                System.out.println("Success");
+            }
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void insertRecord(String firstName,String lastName){
+        try {
+            String sqlQuery = "INSERT INTO user(username,password) VALUES(?,?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+
+
+            int noOfRowsInserted = preparedStatement.executeUpdate();
+            if(noOfRowsInserted>0){
+                System.out.println(noOfRowsInserted + " rows inserted!");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateRecord(int id,String username,String password){
+        try {
+            String sqlQuery = "UPDATE student SET username=?,password=? WHERE id=?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
+            preparedStatement.setString(1,username);
+            preparedStatement.setString(2,password);
+            preparedStatement.setInt(3,id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteRecord(int id){
+        try {
+            String sqlQuery = "DELETE from user WHERE id=?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
+            preparedStatement.setInt(1,id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ResultSet getRecord(int id){
+        try {
+            String sqlQuery = "SELECT * FROM user where id=?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
+            preparedStatement.setInt(1,id);
+            ResultSet result = preparedStatement.executeQuery();
+            return result;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public ResultSet getRecords(){
+        try {
+            String sqlQuery = "SELECT * FROM user";
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(sqlQuery);
+            return result;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
